15946876fe3f86310eac315af6571605f5a99729
Guille <gcabrera@dim.uchile.cl>
2014-11-18 16:49:30 -0300

    Add intrinsic and observable parameters to command line.

--- lib/calculate_bias.py
+++ lib/calculate_bias.py
@@ -27,28 +27,26 @@ from data_analysers.BiasAnalyser import *
 
 parser = argparse.ArgumentParser(description='Calculate labeling bias of a data-set.')
 
-parser.add_argument("table_file", metavar = "table_file", type = file, nargs = 1, 
+parser.add_argument("table_file",
                     help = "table with variables and labels.")
 parser.add_argument("label_name", help = "name of the column containing the labels.")
-parser.add_argument("--number_objects", metavar = "N", type = int, nargs = 1, 
+parser.add_argument("--number_objects", metavar = "N", type = int, default = 50000, 
                     help = "number of objects to be used for calculating the bias.")
+parser.add_argument("--int_pars", default = ["petroRad_r_kpc","absPetroMag_r"], 
+                    nargs = 2,
+                    help = "Name of the columns used as intrinsic parameters.")
+parser.add_argument("--obs_pars", default = ["z", "corrMag_r", "petroRad_r_psf"],
+                    help = "Name of the columns used as observable parameters.")
 
 args = parser.parse_args()
-print args.label_name 
-if (args.number_objects):
-    print "N"
 
-exit()
+N = args.number_objects
+field_r = args.int_pars[0]
+field_c = args.int_pars[1]
+fields_beta = args.obs_pars
+classf = args.label_name
 
-field_r = "petroRad_r_kpc"
-field_c = "absPetroMag_r"
-fields_beta = ["z", "corrMag_r", "petroRad_r_psf"]
-classf = "class_SVM"
-classf = sys.argv[2]
-N = -1843
-N = 50000
-
-dataAn = BiasAnalyser (sys.argv[1])
+dataAn = BiasAnalyser (args.table_file)
 
 print "E = ", (dataAn.tbdata.field(classf) == 1).sum()
 print "S = ", (dataAn.tbdata.field(classf) == 2).sum()
